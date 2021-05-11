ae7928b5164c77b008d483515219d09d159d0b64
AngelCurls <stefany_199829@hotmail.com>
2021-05-11 14:24:53 -0600

    new

--- app/src/main/java/com/example/smarthometec_appmovil/UserInformation.java
+++ app/src/main/java/com/example/smarthometec_appmovil/UserInformation.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class UserInformation extends AppCompatActivity {
 
+    //nada
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
